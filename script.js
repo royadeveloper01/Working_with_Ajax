f6d8f9136f5fb36bf584169adfe5dddb25d50ab5
Chimaobi Prince <royadeveloper01@gmail.com>
2021-02-05 17:10:41 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,51 @@
+var apiUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+var xhrBtn = document.querySelector('#xhr');
+var fetchBtn = document.querySelector('#fetch');
+var axiosBtn = document.querySelector('#axios');
+
+var dispQuotes = document.querySelector("#quotes");
+
+// Making requests with XHR
+xhrBtn.addEventListener('click', function() {
+    var XHR = new XMLHttpRequest();
+    XHR.open('GET',apiUrl);
+    XHR.onreadystatechange = function() {
+        if(XHR.readyState === 4 && XHR.status === 200) {
+            var data = JSON.parse(XHR.responseText)
+            dispQuotes.innerText = data[0]
+        }
+    }
+    XHR.send();
+})
+
+// Making requests with FETCH
+fetchBtn.addEventListener('click', function() {
+    fetch(apiUrl)
+    .then(function(res) {
+        res.json().then(function(data) {
+            dispQuotes.innerText = data[0];
+        })
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+});
+
+// Making requests with JQUERY
+$('#jquery').click(function() {
+    $.getJSON(apiUrl).then(function(data) {
+        $('#quotes').text(data[0])
+    })
+});
+
+// Making requests with AXIOS
+axiosBtn.addEventListener('click', function() {
+    axios.get(apiUrl)
+    .then(res => {
+        var data = res.data[0];
+        dispQuotes.innerText = data;
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
+})
